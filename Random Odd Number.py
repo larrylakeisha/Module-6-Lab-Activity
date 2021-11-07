f3fe6b40b3e8e06de7e1a33feb6b253fca734f53
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-11-07 14:12:40 -0600

    Update Random Odd Number.py

--- Random Odd Number.py
+++ Random Odd Number.py
@@ -4,6 +4,7 @@
 # Program 2 prints an odd integer between the numbers 0 and 100
 
 import random
+'''What if the generated number is not odd number?'''
 oddnum = random.randrange(1, 100)
 if oddnum % 2 != 0:
     print(oddnum)
